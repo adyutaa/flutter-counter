03a57ff47a7720f2850a8a1ca13960ea87e4a9d9
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2024-01-12 17:01:03 +0700

    Flutter counter app mitchkoko

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -14,29 +14,36 @@ class _CounterPageState extends State<CounterPage> {
   //method
   void _incrementCounter() {
     // setstate rebuilds the widgets
-    setState(() { // the value will change depending on the state(new value in example)
+    setState(() {
+      // the value will change depending on the state(new value in example)
       _counter++;
     });
-    
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("You pushed the button this many times: "),
-            Text(
-              _counter.toString(),
-              style: TextStyle(fontSize: 50),
-            ),
-
-            ElevatedButton(
-              onPressed: _incrementCounter, 
-              child:Text("Button"), )
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blueAccent, Colors.purpleAccent])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("You pushed the button this many times: "),
+              Text(
+                _counter.toString(),
+                style: TextStyle(fontSize: 50),
+              ),
+              ElevatedButton(
+                onPressed: _incrementCounter,
+                child: Text("Button"),
+              )
+            ],
+          ),
         ),
       ),
     );
